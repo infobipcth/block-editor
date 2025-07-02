d2410ba8d2d4faf83af38103324dccf10b11045d
Denis Žoljom <denis.zoljom@infobip.com>
2025-07-02 11:34:29 +0200

    Fix the optimize CSS script

--- bin/optimize-css.php
+++ bin/optimize-css.php
@@ -9,46 +9,47 @@ $outputFile = "$projectRoot/src/styles.scss";
 // Create _dependencies.scss file if it doesn't exist.
 file_put_contents($outputFile, '');
 
-// Process styles.scss
-// Process dependencies.scss
-// Process custom.scss
-
-$processedContent = file_get_contents($stylesDir . "/styles.scss") . "\n";
+// Initialize processed content
+$processedContent = '';
+if (file_exists($stylesDir . "/styles.scss")) {
+    $processedContent = file_get_contents($stylesDir . "/styles.scss") . "\n";
+}
 
 $depFiles = glob("$dependencyDir/*.scss");
 foreach ($depFiles as $file) {
-	$content = file($file);
+    if (!file_exists($file)) {
+        continue;
+    }
+    $content = file($file);
 
-	// Process each line of the file
-	$partialPath = '';
-	foreach ($content as $line) {
-		if (str_starts_with($line, '@use')) {
-			$path = str_replace('@use "../../../', '', $line);
-			$path = trim(str_replace('";', '', $path));
+    // Process each line of the file
+    foreach ($content as $line) {
+        if (str_starts_with($line, '@use')) {
+            $path = str_replace('@use "../../../', '', $line);
+            $path = trim(str_replace('";', '', $path));
+            $sanitizedPath = realpath("$projectRoot/$path");
+            if ($sanitizedPath && str_starts_with($sanitizedPath, $projectRoot)) {
+                $fileContent = file($sanitizedPath);
 
-			$fileContent = file("$projectRoot/$path");
-
-			// Clean any @use and @import statements
-			foreach ($fileContent as $innerLine) {
-				if (str_contains($innerLine, '@use')) {
-					continue;
-				}
-
-				if (str_contains($innerLine, '@import')) {
-					continue;
-				}
-
-				$processedContent .= $innerLine . "\n";
-			}
-		} else {
-			// Copy other content as is
-			$processedContent .= $line . "\n";
-		}
-	}
+                // Clean any @use and @import statements
+                foreach ($fileContent as $innerLine) {
+                    if (str_contains($innerLine, '@use') || str_contains($innerLine, '@import')) {
+                        continue;
+                    }
+                    $processedContent .= $innerLine . "\n";
+                }
+            }
+        } else {
+            // Copy other content as is
+            $processedContent .= $line . "\n";
+        }
+    }
 }
 
-$processedContent .= file_get_contents($stylesDir . "/custom.scss") . "\n";
+if (file_exists($stylesDir . "/custom.scss")) {
+    $processedContent .= file_get_contents($stylesDir . "/custom.scss") . "\n";
+}
 
 // Remove empty lines
 $cleanContent = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $processedContent);
-file_put_contents($outputFile, $cleanContent);
+file_put_contents($outputFile, $cleanContent, FILE_APPEND);
